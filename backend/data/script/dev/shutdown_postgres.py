4dd0cc28e125daf83c34dcaa72f5ad97ab63f691
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2022-02-19 14:54:30 -0300

    postgres shutdown kubernetes

---
+++ backend/data/script/dev/shutdown_postgres.py
@@ -0,0 +1,17 @@
+import kubernetes as k8s
+import helper
+
+
+def main():
+    k8s.config.load_kube_config()
+    with k8s.client.ApiClient() as k8s_client:
+        k8s_apps_v1 = k8s.client.AppsV1Api(k8s_client)
+        k8s_core_v1 = k8s.client.CoreV1Api(k8s_client)
+        k8s_networking_v1 = k8s.client.NetworkingV1Api(k8s_client)
+
+        helper.delete_service(k8s_core_v1, 'postgres')
+        helper.delete_deployment(k8s_apps_v1, 'postgres-deployment')
+
+
+if __name__ == '__main__':
+    main()
