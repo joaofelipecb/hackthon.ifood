3d849a89e1c3827c774c53a13416a18d55138c8f
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2022-02-19 14:50:02 -0300

    created postgres kubernet shutdown

--- backend/data/script/dev/helper.py
+++ backend/data/script/dev/helper.py
@@ -16,3 +16,89 @@ def apply_kubernetes_or_handle_error(k8s_client, resource_file):
                       ))
             else:
                 raise e
+
+
+def delete_ingress(k8s_core_v1, object_name):
+    try:
+        k8s_core_v1.delete_namespaced_ingress(
+                object_name,
+                namespace = 'default'
+                )
+        print(f'Ingress {object_name} deleted')
+    except k8s.client.exceptions.ApiException as e:
+        if e.reason == 'Not Found':
+            print((
+                   f'Ingress {object_name} not deleted '
+                   'because it was not found'
+                  ))
+        else:
+            raise e
+
+
+def delete_deployment(k8s_apps_v1, object_name):
+    try:
+        k8s_apps_v1.delete_namespaced_deployment(
+                object_name,
+                namespace = 'default'
+                )
+        print(f'Deployment {object_name} deleted')
+    except k8s.client.exceptions.ApiException as e:
+        if e.reason == 'Not Found':
+            print((
+                   f'Deployment {object_name} not deleted '
+                   'because it was not found'
+                  ))
+        else:
+            raise e
+
+
+def delete_service(k8s_core_v1, object_name):
+    try:
+        k8s_core_v1.delete_namespaced_service(
+                object_name,
+                namespace = 'default'
+                )
+        print(f'Service {object_name} deleted')
+    except k8s.client.exceptions.ApiException as e:
+        if e.reason == 'Not Found':
+            print((
+                   f'Service {object_name} not deleted '
+                   'because it was not found'
+                  ))
+        else:
+            raise e
+
+
+def delete_persistent_volume_claim(k8s_core_v1, object_name):
+    try:
+        k8s_core_v1.delete_namespaced_persistent_volume_claim(
+                object_name,
+                namespace = 'default'
+                )
+        print(f'Persistent Volume Claim {object_name} deleted')
+    except k8s.client.exceptions.ApiException as e:
+        if e.reason == 'Not Found':
+            print((
+                   f'Persistent Volume Claim {object_name} not deleted '
+                   'because it was not found'
+                  ))
+        else:
+            raise e
+
+
+def delete_persistent_volume(k8s_core_v1, object_name):
+    try:
+        k8s_core_v1.delete_persistent_volume(
+                object_name
+                )
+        print(f'Persistent Volume {object_name} deleted')
+    except k8s.client.exceptions.ApiException as e:
+        if e.reason == 'Not Found':
+            print((
+                   f'Persistent Volume {object_name} not deleted '
+                   'because it was not found'
+                  ))
+        else:
+            raise e
+
+
--- backend/data/script/dev/shutdown.py
+++ backend/data/script/dev/shutdown.py
@@ -1,87 +1,5 @@
 import kubernetes as k8s
-
-def delete_ingress(k8s_core_v1, object_name):
-    try:
-        k8s_core_v1.delete_namespaced_ingress(
-                object_name,
-                namespace = 'default'
-                )
-        print(f'Ingress {object_name} deleted')
-    except k8s.client.exceptions.ApiException as e:
-        if e.reason == 'Not Found':
-            print((
-                   f'Ingress {object_name} not deleted '
-                   'because it was not found'
-                  ))
-        else:
-            raise e
-
-
-def delete_deployment(k8s_apps_v1, object_name):
-    try:
-        k8s_apps_v1.delete_namespaced_deployment(
-                object_name,
-                namespace = 'default'
-                )
-        print(f'Deployment {object_name} deleted')
-    except k8s.client.exceptions.ApiException as e:
-        if e.reason == 'Not Found':
-            print((
-                   f'Deployment {object_name} not deleted '
-                   'because it was not found'
-                  ))
-        else:
-            raise e
-
-
-def delete_service(k8s_core_v1, object_name):
-    try:
-        k8s_core_v1.delete_namespaced_service(
-                object_name,
-                namespace = 'default'
-                )
-        print(f'Service {object_name} deleted')
-    except k8s.client.exceptions.ApiException as e:
-        if e.reason == 'Not Found':
-            print((
-                   f'Service {object_name} not deleted '
-                   'because it was not found'
-                  ))
-        else:
-            raise e
-
-
-def delete_persistent_volume_claim(k8s_core_v1, object_name):
-    try:
-        k8s_core_v1.delete_namespaced_persistent_volume_claim(
-                object_name,
-                namespace = 'default'
-                )
-        print(f'Persistent Volume Claim {object_name} deleted')
-    except k8s.client.exceptions.ApiException as e:
-        if e.reason == 'Not Found':
-            print((
-                   f'Persistent Volume Claim {object_name} not deleted '
-                   'because it was not found'
-                  ))
-        else:
-            raise e
-
-
-def delete_persistent_volume(k8s_core_v1, object_name):
-    try:
-        k8s_core_v1.delete_persistent_volume(
-                object_name
-                )
-        print(f'Persistent Volume {object_name} deleted')
-    except k8s.client.exceptions.ApiException as e:
-        if e.reason == 'Not Found':
-            print((
-                   f'Persistent Volume {object_name} not deleted '
-                   'because it was not found'
-                  ))
-        else:
-            raise e
+import helper
 
 
 def main():
@@ -91,11 +9,10 @@ def main():
         k8s_core_v1 = k8s.client.CoreV1Api(k8s_client)
         k8s_networking_v1 = k8s.client.NetworkingV1Api(k8s_client)
 
-        delete_ingress(k8s_networking_v1, 'jenkins-ingress')
-        delete_service(k8s_core_v1, 'jenkins')
-        delete_deployment(k8s_apps_v1, 'jenkins-deployment')
-        #delete_persistent_volume_claim(k8s_core_v1, 'jenkins-pv-claim')
-        #delete_persistent_volume(k8s_core_v1, 'jenkins-pv-volume')
+        helper.delete_ingress(k8s_networking_v1, 'jenkins-ingress')
+        helper.delete_service(k8s_core_v1, 'jenkins')
+        helper.delete_deployment(k8s_apps_v1, 'jenkins-deployment')
+
 
 if __name__ == '__main__':
     main()
